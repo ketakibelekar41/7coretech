ad4ebdfcaea2939144f21152556ba72498a4157e
ketakibelekar41 <159541558+ketakibelekar41@users.noreply.github.com>
2024-07-30 11:57:12 +0530

    Update BlogPostList.js

--- src/Components/BlogPostList.js
+++ src/Components/BlogPostList.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './BlogPostList.css'; // Import the CSS file
+import './BlogPostList.css';
 
 const BlogPostList = () => {
-    //store data by initializing state
+
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-06-25&sortBy=publishedAt&apiKey=07e83f02f11c4cadab7345f30d6c3736')
-            .then((res) => res.json()) //parse response in json format
-            .then((data) => setPosts(data.articles)) //updating setPosts by fetched data
-            .catch((e) => console.log(e, "Error"));// shows error when failed to fetch
-    }, []);// load data at first time load
+            .then((res) => res.json()) 
+            .then((data) => setPosts(data.articles)) 
+            .catch((e) => console.log(e, "Error"));
+    }, []);
 
     return (
-        <div className="container"> {/* Apply the container class */}
-            {posts?.map((post, index) => ( //maping the post data 
-                <div key={index} className="post"> {/* Apply the post class */}
+        <div className="container"> 
+            {posts?.map((post, index) => (
+                <div key={index} className="post"> 
                     <h1>{post.title}</h1>
-                    <Link to={`/post/${index}`}>View Details</Link> {/* navigate to postpage with particular postID data */}
+                    <Link to={`/post/${index}`}>View Details</Link> 
                 </div>
             ))}
         </div>
