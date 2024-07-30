10f2798ce2c53ce7efe7c0a7d95894e2e2c1e1b7
ketakibelekar41 <159541558+ketakibelekar41@users.noreply.github.com>
2024-07-30 11:53:09 +0530

    Update BlogPostItem.js

--- src/Components/BlogPostItem.js
+++ src/Components/BlogPostItem.js
@@ -2,44 +2,44 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 const BlogPostItem = () => {
-    //extract id from URL
+    
     const { id } = useParams();
-    //store the blogpost data by initializing state
+  
     const [post, setPost] = useState(null);
-    //to show the loader
+   
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {  // useEffect will render the data at first time load as well as when id get changes
+    useEffect(() => {  
         const fetchPost = async () => {
             try {
                 const res = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2024-06-25&sortBy=publishedAt&apiKey=07e83f02f11c4cadab7345f30d6c3736`);
-                const data = await res.json(); //parse res in JSON format
-                const postId = parseInt(id, 10); // Convert id to an integer
-                setPost(data.articles[postId]); //setPost used to update data with particular postId
-                setLoading(false); //update loading status
+                const data = await res.json(); 
+                const postId = parseInt(id, 10); 
+                setPost(data.articles[postId]); 
+                setLoading(false);
             } catch (error) {
-                console.error('Error fetching data:', error);// shows errror when failed to fetch
-                setLoading(false); //update loading status
+                console.error('Error fetching data:', error);
+                setLoading(false); 
             }
         };
 
         fetchPost();
-    }, [id]); // give dependency to render the data when dependency change
+    }, [id]); 
 
     if (loading) {
-        return <div>Loading...</div>; //show loading before rendering the posts data
+        return <div>Loading...</div>; 
     }
 
     if (!post) {
-        return <div>Post not found</div>; //if there is no post then show message
+        return <div>Post not found</div>; 
     }
 
     return (
         <div>
-            <Link to="/">Back to list</Link> {/* navigate back to postlist home page */}
-            <h1>{post.title}</h1> {/* render titles */}
-            <img src={post.urlToImage} alt={post.title} /> {/* render image and alternative post title */}
-            <p>{post.content}</p> {/* render the post content */}
+            <Link to="/">Back to list</Link> 
+            <h1>{post.title}</h1> 
+            <img src={post.urlToImage} alt={post.title} /> 
+            <p>{post.content}</p> 
         </div>
     );
 };
